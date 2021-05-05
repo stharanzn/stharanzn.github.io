ac77053027e71534a35bc2758fe66c3f02b2b55a
stharanzn <38763905+stharanzn@users.noreply.github.com>
2021-05-05 15:18:59 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,7 +4,42 @@ const overlay = document.querySelector('.overlay');
 const menu = document.querySelector('#mobile_menu');
 const body = document.querySelector('body');
 
+const toggle = document.querySelector('#toggling');
+const toggleClass = document.querySelector('.header__logo');
+
 const buttons =  document.querySelectorAll('.invite');
+
+toggle.addEventListener('click',function(){
+    if(body.classList.contains('light') & body.classList.contains('light__lightBody')){
+        body.classList.remove('light');
+        body.classList.remove('light__body');
+        toggleClass.classList.add('dark__toggle');
+        body.classList.add('dark');
+        body.classList.add('dark__darkBody');
+    }else{
+        body.classList.remove('dark');
+        body.classList.remove('dark__darkBody');
+        toggleClass.classList.add('light++toggle');
+        body.classList.add('light');
+        body.classList.add('light__lightBody');
+    }
+});
+
+function toggleMode(){
+    if(body.classList.contains('light') & body.classList.contains('light__lightBody')){
+        body.classList.remove('light');
+        body.classList.remove('light__body');
+        toggleClass.classList.add('dark__toggle');
+        body.classList.add('dark');
+        body.classList.add('dark__darkBody');
+    }else{
+        body.classList.remove('dark');
+        body.classList.remove('dark__darkBody');
+        toggleClass.classList.add('light++toggle');
+        body.classList.add('light');
+        body.classList.add('light__lightBody');
+    }
+}
 
 btnhamburger.addEventListener('click',function(){
     if(header.classList.contains('open')){
