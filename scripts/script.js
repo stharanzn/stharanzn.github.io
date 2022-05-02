cf63085cfc8657b85dd6a835406e417a2f3daba6
stharanzn <stharanzn1705@gmail.com>
2022-05-02 18:24:54 +0530

    adding developing portfolio page

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+
+// const url = 'https://github-repository-beagle.p.rapidapi.com/version';
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Host': 'github-repository-beagle.p.rapidapi.com',
+//     'X-RapidAPI-Key': 'df06079815msh4215d90aa2d29aap1214f2jsn2c88284cca9b'
+//   }
+// };
+
+// fetch(url, options)
+// 	.then(res => res.json())
+// 	.then(json => {
+//         console.log(json)
+//         document.getElementById('response').innerHTML = json.ApiVersion})
+// 	.catch(err => console.error('error:' + err));
